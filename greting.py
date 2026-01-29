591e41b3bdd446f3e992d12aa0bd8c1543ae10aa
shruti2400 <128717070+shruti2400@users.noreply.github.com>
2026-01-29 12:25:00 +0530

    Add files via upload

---
+++ greting.py
@@ -0,0 +1,8 @@
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+def greetings(f_name,l_name):
+    print(f" hello {f_name} {l_name} welcome to the program")
+
+print(greetings(f_name,l_name))
+
